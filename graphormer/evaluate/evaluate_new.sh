5175f056e39953d20ef3287341612ebf0db263fd
Micholms <156813532+Micholms@users.noreply.github.com>
2024-12-09 16:27:44 +0100

    graphormer_uploaded
    graphormer_uploaded

---
+++ graphormer/evaluate/evaluate_new.sh
@@ -0,0 +1,20 @@
+python Graphormer-IR/graphormer/evaluate/evaluate.py \
+    --user-dir Graphormer-IR/graphormer \
+    --num-workers 6 \
+    --ddp-backend=legacy_ddp \
+	--user-data-dir Graphormer-IR/graphormer/evaluate/testing_dataset \
+	--dataset-name IR_test \
+    --task graph_prediction \
+	--criterion sid \
+	--arch graphormer_base \
+    --encoder-layers 4 \
+    --encoder-embed-dim  2100 \
+    --encoder-ffn-embed-dim 2100 \
+    --encoder-attention-heads 210 \
+    --mlp-layers 3 \
+    --batch-size 32 \
+    --num-classes 1801 \
+    --save-dir $1 \
+    --split train \
+    --dataset-source $2\
+
