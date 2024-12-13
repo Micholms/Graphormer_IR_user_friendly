97b73a4ba9341521bb7083daa5bfc49d44957a5d
Micholms <156813532+Micholms@users.noreply.github.com>
2024-12-13 09:56:48 +0100

    Update Interactive_training.sh

--- Interactive_training.sh
+++ Interactive_training.sh
@@ -10,9 +10,18 @@ echo "                                                 Answer all questions acco
 
 "
 
-echo '                                                 Enter base model path'
-echo -n "                                                   Model path: "
+echo '                                                 Enter base model DIRECTORY'
+echo -n "                                                   Model DIRECTORY: "
 read -r model
+
+until  test -d ./$model
+do
+    echo -n "                                                 Model DIRECTORY doesn't exist. Enter model DIRECTORY path: "
+    read -r model
+done
+echo $'\033[1m                                                 Directory exists.
+
+'
 
 echo "                                                 Enter learining rate"
 echo -n "                                                   LR: "
@@ -27,17 +36,27 @@ echo -n "                                                   Save path: "
 read -r save
 
 
-echo "                                                 Enter path to dataset to train on"
-echo -n "                                                   Dataset path: "
+echo "                                                 Enter DIRECTORY to dataset to train on"
+echo -n "                                                   Dataset DIRECTORY: "
 read -r dataset
+
+until  test -d ./$dataset
+do
+    echo -n "                                                 Dataset DIRECTORY doesn't exist. Enter DIRECTORY to dataset: "
+    read -r dataset
+done
+echo $'\033[1m                                                 Directory exists.
+
+'
 
 
 cp -r $dataset/*.csv ./
 
 
-bash Graphormer-IR/examples/property_prediction/Train_IR_model.sh $lr $epochs $model $save 
+bash Graphormer-IR/examples/property_prediction/Train_IR_model.sh $lr $epochs $model $save
 
 rm testing_dataset.csv
 rm training_dataset.csv
 rm train_indices.csv
 rm valid_indices.csv
+
